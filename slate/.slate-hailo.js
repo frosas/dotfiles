ccf77cd7e64ad004ff4bfc5f2b8bd8c006a5e4a2
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-07-11 20:11:56 +0100

    Slate configuration for Hailo

---
+++ slate/.slate-hailo.js
@@ -0,0 +1,56 @@
+slate.log('Loading config')
+
+var screens = {
+    laptop: '1280x800',
+    thunderbolt: '2560x1440'
+}
+
+var windowMargin = 20
+
+var operations = {}
+var layouts = {}
+
+operations.moveToLeft = function(screen) {
+    return slate.operation('move', {
+        screen: screen,
+        x: 'screenOriginX + ' + windowMargin,
+        y: 'screenOriginY + ' + windowMargin,
+        width: 'screenSizeX / 2 - ' + windowMargin * 1.5,
+        height: 'screenSizeY - ' + windowMargin * 2
+    })
+}
+
+operations.moveToRight = function(screen) {
+    return slate.operation('move', {
+        screen: screen,
+        x: 'screenOriginX + screenSizeX / 2 + ' + windowMargin * .5,
+        y: 'screenOriginY + ' + windowMargin, 
+        width: 'screenSizeX / 2 - ' + windowMargin * 1.5,
+        height: 'screenSizeY - ' + windowMargin * 2
+    })
+}
+
+layouts.twoScreens = slate.layout('twoScreens', {
+    'Google Chrome': {
+        operations: operations.moveToLeft(screens.thunderbolt)
+    },
+    PhpStorm: {
+        operations: operations.moveToRight(screens.thunderbolt)
+    }
+})
+
+operations.layout = {
+    twoScreens: slate.operation('layout', {
+        name: layouts.twoScreens
+    })
+}
+
+slate.bind('s:ctrl', function() {
+    slate.log('Setting default layout')
+    operations.layout.twoScreens.run()
+    slate.log('Done')
+})
+
+slate.default(2, layouts.twoScreens)
+
+slate.log('Config loaded')
