7b568f4f81952ee3fb64b1ea2a37b164253c28be
Francesc Rosàs <francescrosasbosque@gmail.com>
2014-11-22 21:02:21 +0000

    Add semicolons

--- home/.slate.js
+++ home/.slate.js
@@ -1,31 +1,31 @@
 // Docs at https://github.com/jigish/slate/wiki/JavaScript-Configs
 
-slate.log('Loading config') // Good to know this file was found
+slate.log('Loading config'); // To know whether this file was found
 
 var getScreens = function() {
-    var screens = {}
-    var externalScreensCount = 0
+    var screens = {};
+    var externalScreensCount = 0;
     slate.eachScreen(function(screen) {
-        // Screen IDs are ordered from left to right so external-0 stays on the left 
+        // Screen IDs are ordered from left to right so external-0 stays on the left
         // of external-1
         var name = screen.isMain() ? 'main' : (function() {
-            var id = 'external-' + externalScreensCount 
-            externalScreensCount++
-            return id
-        })()
-        screens[name] = screen.id()
-    })
-    return screens
-}
+            var id = 'external-' + externalScreensCount ;
+            externalScreensCount++;
+            return id;
+        })();
+        screens[name] = screen.id();
+    });
+    return screens;
+};
 
 var getScreen = function(name) {
-    var screens = getScreens()
-    return name in screens ? screens[name] : screens.main
-}
+    var screens = getScreens();
+    return name in screens ? screens[name] : screens.main;
+};
 
 // Operations
 
-var operations = {}
+var operations = {};
 
 /**
  * Applies the operation or list of operations
@@ -33,14 +33,14 @@ var operations = {}
 operations.apply = function(window, operation) {
     if (Array.isArray(operation)) {
         operation.forEach(function(subOperation) {
-            operations.apply(window, subOperation)
-        })
+            operations.apply(window, subOperation);
+        });
     } else {
-        window.doOperation(operation)
+        window.doOperation(operation);
     }
-}
+};
 
-var leftRegionWidth = 'screenSizeX * 0.4'
+var leftRegionWidth = 'screenSizeX * 0.4';
 
 operations.toLeft = function() {
     return slate.operation('move', {
@@ -48,8 +48,8 @@ operations.toLeft = function() {
         y: 'screenOriginY',
         width: leftRegionWidth,
         height: 'screenSizeY'
-    })
-}
+    });
+};
 
 operations.toRight = function() {
     return slate.operation('move', {
@@ -57,8 +57,8 @@ operations.toRight = function() {
         y: 'screenOriginY',
         width: 'screenSizeX - (' + leftRegionWidth + ')',
         height: 'screenSizeY'
-    })
-}
+    });
+};
 
 operations.toTop = function() {
     return slate.operation('move', {
@@ -66,8 +66,8 @@ operations.toTop = function() {
         y: 'screenOriginY',
         width: 'screenSizeX',
         height: 'screenSizeY / 2'
-    })
-}
+    });
+};
 
 operations.toBottom = function() {
     return slate.operation('move', {
@@ -75,8 +75,8 @@ operations.toBottom = function() {
         y: 'screenOriginY / 2',
         width: 'screenSizeX',
         height: 'screenSizeY / 2'
-    })
-}
+    });
+};
 
 operations.maximize = function() {
     return slate.operation('move', {
@@ -84,122 +84,122 @@ operations.maximize = function() {
         y: 'screenOriginY',
         width: 'screenSizeX',
         height: 'screenSizeY'
-    })
-}
+    });
+};
 
 var runAppOperations = function(app, appOperations) {
-    app.eachWindow(function(window) { operations.apply(window, appOperations) })
-}
+    app.eachWindow(function(window) { operations.apply(window, appOperations); });
+};
 
 var getAppsByName = function(names) {
-    var apps = []
+    var apps = [];
     slate.eachApp(function(app) {
-        if (names.some(function(name) { return name == app.name() })) {
-            apps.push(app)
+        if (names.some(function(name) { return name == app.name(); })) {
+            apps.push(app);
         }
-    })
-    return apps
-}
+    });
+    return apps;
+};
 
 var getAvailableAppsByName = function(names) {
-    return getAppsByName(names).filter(function(app) { return app })
-}
+    return getAppsByName(names).filter(function(app) { return app; });
+};
 
 var runAppOperationsByNames = function(names, appOperations) {
     getAvailableAppsByName(names).forEach(function(app) {
-        runAppOperations(app, appOperations)
-    })
-}
+        runAppOperations(app, appOperations);
+    });
+};
 
 var layoutAll = function() {
-    var screenCount = slate.screenCount()
+    var screenCount = slate.screenCount();
 
-    slate.log('Laying out (' + screenCount + ' screens)')
+    slate.log('Laying out (' + screenCount + ' screens)');
 
     runAppOperationsByNames(['iTerm', 'Terminal'], (function() {
         switch (screenCount) {
-            case 1: return operations.maximize()
+            case 1: return operations.maximize();
             case 2:
-            case 3: 
+            case 3:
                 return [
                     slate.operation('throw', {screen: getScreen('external-0')}),
                     operations.toLeft()
-                ]
+                ];
         }
-    })())
+    })());
 
     runAppOperationsByNames(['Google Chrome', 'Firefox', 'Safari', 'SourceTree'], (function() {
         switch (screenCount) {
-            case 1: return operations.maximize()
+            case 1: return operations.maximize();
             case 2:
             case 3: return [
                 slate.operation('throw', {screen: getScreen('external-0')}),
                 operations.toRight()
-            ]
+            ];
         }
-    })())
+    })());
 
     runAppOperationsByNames(['PhpStorm', 'MacVim', 'Atom', 'Eclipse'], [
         slate.operation('throw', {screen: getScreen('main')}),
         operations.maximize()
-    ])
+    ]);
 
     runAppOperationsByNames(['HipChat'], (function() {
         switch (screenCount) {
             case 1:
-            case 2: 
+            case 2:
                 return [
                     slate.operation('throw', {screen: getScreen('main')}),
                     operations.maximize()
-                ]
+                ];
             case 3:
                 return [
                     slate.operation('throw', {screen: getScreen('external-1')}),
                     operations.toTop()
-                ]
+                ];
         }
-    })())
+    })());
 
     runAppOperationsByNames(['Spotify'], (function() {
         switch (screenCount) {
             case 1:
-            case 2: 
+            case 2:
                 return [
                     slate.operation('throw', {screen: getScreen('main')}),
                     operations.maximize()
-                ]
+                ];
             case 3:
                 return [
                     slate.operation('throw', {screen: getScreen('external-1')}),
                     operations.toBottom()
-                ]
+                ];
         }
-    })())
-}
+    })());
+};
 
-slate.default(1, layoutAll)
-slate.default(2, layoutAll)
-slate.default(3, layoutAll)
+slate.default(1, layoutAll);
+slate.default(2, layoutAll);
+slate.default(3, layoutAll);
 
 // Keybindings
 
 var bind = function(keystroke, keystrokeOperations) {
-    slate.bind(keystroke + ':shift,ctrl', function(window) { 
-        slate.log('Triggering \'' + keystroke + '\'')
-        if (keystrokeOperations instanceof Function) keystrokeOperations()
-        else operations.apply(window, keystrokeOperations) 
-    })
-}
+    slate.bind(keystroke + ':shift,ctrl', function(window) {
+        slate.log('Triggering \'' + keystroke + '\'');
+        if (keystrokeOperations instanceof Function) keystrokeOperations();
+        else operations.apply(window, keystrokeOperations);
+    });
+};
 
-bind('r', slate.operation('relaunch'))
-bind('a', layoutAll)
-bind('m', operations.maximize())
-bind('h', operations.toLeft())
-bind('l', operations.toRight())
-bind('k', operations.toTop())
-bind('j', operations.toBottom())
-bind('1', slate.operation('throw', {screen: getScreen('main')}))
-bind('2', slate.operation('throw', {screen: getScreen('external-0')}))
-bind('3', slate.operation('throw', {screen: getScreen('external-1')}))
+bind('r', slate.operation('relaunch'));
+bind('a', layoutAll);
+bind('m', operations.maximize());
+bind('h', operations.toLeft());
+bind('l', operations.toRight());
+bind('k', operations.toTop());
+bind('j', operations.toBottom());
+bind('1', slate.operation('throw', {screen: getScreen('main')}));
+bind('2', slate.operation('throw', {screen: getScreen('external-0')}));
+bind('3', slate.operation('throw', {screen: getScreen('external-1')}));
 
-slate.log('Config loaded') // Good to know everything loaded fine
+slate.log('Config loaded'); // Good to know everything loaded fine
